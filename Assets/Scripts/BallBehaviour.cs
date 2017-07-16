308ca38230c4b570e08876608e31a644ddb3ff39
Tyler Kuipers <tkuipers123@gmail.com>
2017-07-16 17:46:46 -0300

    Initial COmmit of First Unity Project

---
+++ Assets/Scripts/BallBehaviour.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBehaviour : MonoBehaviour {
+    public float speed = 30;
+
+    private Rigidbody2D rBod;
+
+
+	// Use this for initialization
+	void Start () {
+        rBod = GetComponent<Rigidbody2D>();
+        rBod.velocity = Vector2.right * speed;
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        string collideName = collision.gameObject.name;
+        if (collideName.Equals("LeftPlayer")) {
+            handlePaddleHit(collision);
+        }
+        else if (collideName.Equals("RightPlayer")) {
+            handlePaddleHit(collision);
+        }
+        else if (collideName.Equals("Top")) {
+
+        }
+        else if (collideName.Equals("Bottom")) {
+
+        }
+        else if (collideName.Equals("LeftGoal")) {
+            rBod.velocity = new Vector2(0, 0);
+        }
+        else if (collideName.Equals("RightGoal")) {
+            rBod.velocity = new Vector2(0, 0);
+        }
+    }
+
+    private void handlePaddleHit(Collision2D collision) {
+        float y = ballHitPaddleWhere(transform.position, collision.transform.position, collision.collider.bounds.size.y);
+        Vector2 dir = new Vector2();
+        if (collision.gameObject.name.Equals("LeftPlayer")) {
+            dir = new Vector2(1, y).normalized;
+        }
+
+        if (collision.gameObject.name.Equals("RightPlayer")) {
+            dir = new Vector2(-1, y).normalized;
+        }
+
+        rBod.velocity = dir * speed;
+    }
+
+    private void handlePaddleHit(Collision collision) {
+       
+    }
+
+    private float ballHitPaddleWhere(Vector2 ball, Vector2 paddle, float y) {
+        return (ball.y - paddle.y) / y;
+    }
+
+}
---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour {
+    public float speed = 30;
+
+    void FixedUpdate() {
+        float vPress = Input.GetAxisRaw("Vertical");
+        float hPress = Input.GetAxisRaw("Horizontal");
+        GetComponent<Rigidbody2D>().velocity = new Vector2(hPress, vPress) * speed;
+
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    }
+}
